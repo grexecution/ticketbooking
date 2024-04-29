e11a7017473ea64e1f732f2a4e785c8f9f966e80
Sviat Dev <matrix.yurinets.sv@gmail.com>
2024-04-29 16:47:18 -0500

    event status

--- app/Http/Controllers/Site/EventController.php
+++ app/Http/Controllers/Site/EventController.php
@@ -12,13 +12,16 @@ class EventController extends Controller
 
     public function index(Request $request) : Renderable
     {
-        $events = Event::all();
+        $events = Event::where('status', Event::STATUS_LIVE)->get();
         return view('site.events.index', compact('events'));
     }
 
     public function show(string $id) : Renderable
     {
         $event = Event::findOrFail($id);
+        if ($event->status !== Event::STATUS_LIVE) {
+            abort(404);
+        }
         return view('site.events.show', compact('event'));
     }
 }
