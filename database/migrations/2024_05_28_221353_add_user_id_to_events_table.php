4324e8fbcbf65152e6377c323d82caf5bad195fa
Sviatoslav Yurynets <matrix.yurinets.sv@gmail.com>
2024-05-28 21:27:06 -0500

    fix deployment

--- database/migrations/2024_05_28_221353_add_user_id_to_events_table.php
+++ database/migrations/2024_05_28_221353_add_user_id_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->after('id');
+            $table->unsignedBigInteger('user_id')->after('id')->nullable();
             $table->foreign('user_id')
                 ->references('id')->on('users')
                 ->onUpdate('cascade')->onDelete('cascade');
