91f330d0b7b661777e9406f4aee64ac3ad9c2011
grexecution <greg.wallner@gmail.com>
2024-06-25 10:55:36 +0200

    Favicon

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}">
     <!-- Dropzone CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet">
     <!-- Select2 CSS -->
